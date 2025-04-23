c512cb05281c5b7ad68779571ff375af93d3796e
RioChurrascaria <diavale274@gmail.com>
2025-04-23 18:57:57 +0200

    Prima versione dell'app Rio Churrascaria

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'rio-churrascaria-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/images/logo.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
